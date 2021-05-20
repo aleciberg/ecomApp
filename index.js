b2b15bc3c5e9a3a78fa06a96cd8e3899d1618e8a
Alec Iberg <ibergalec@gmail.com>
2021-05-20 12:43:36 -0700

    initial commit after some time building

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`
+        <div>
+        <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password"/>
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+         <button>Sign Up</button>
+        </form>
+        </div>
+    `);
+});
+
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email already exists!");
+  }
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords do not match!");
+  }
+  res.send("account created");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
